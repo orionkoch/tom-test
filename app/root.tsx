a2d5a11b682314ce7d85c423fe5d1d8a096390fa
orionkoch <tommysalsa@gmail.com>
2023-02-16 11:14:20 -0500

    4 files modified

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,39 @@
 import {
-	Links,
-	LiveReload,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-} from "@remix-run/react";
-import type { MetaFunction } from "@remix-run/node";
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
+import type { MetaFunction } from '@remix-run/node';
+import { PlasmicRootProvider } from '@plasmicapp/react-web';
 
 export const meta: MetaFunction = () => {
-	return { title: "New Remix App" };
+  return { title: 'New Remix App' };
 };
 
 export default function App() {
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width,initial-scale=1" />
-				<Meta />
-				<Links />
-			</head>
-			<body>
-				<Outlet />
-				<ScrollRestoration />
-				<Scripts />
-				{process.env.NODE_ENV === "development" && <LiveReload />}
-			</body>
-		</html>
-	);
+  return (
+    <PlasmicRootProvider>
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          {process.env.NODE_ENV === 'development' && <LiveReload />}
+        </body>
+      </html>
+    </PlasmicRootProvider>
+  );
 }
+
+// function MyApp() {
+//   return {/* your app here! */};
+// }
